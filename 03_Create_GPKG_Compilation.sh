23bb9cc1d7c4f1d684ac272b5a6f0af87798586a
mia.schumacher <mschumacher@geomar.de>
2025-10-22 14:26:05 +0200

    now uses filelist to create geopackage compikations

--- 03_Create_GPKG_Compilation.sh
+++ 03_Create_GPKG_Compilation.sh
@@ -1,11 +1,12 @@
 # Merge all coverage.gpkg in dst dir into one coverage gpkg compilation
-# Usage: bash Create_GPKG_Compilation platform dst_path
-# platform: one of MERIAN, METEOR, SONNE; dst_path: directory containing all coverage geopackages
+# Usage: bash 03_Create_GPKG_Compilation platform file_list
+# platform: one of MERIAN, METEOR, SONNE; file_list: path/to/platform_gpkg_list
+# Note: file list needs to contains absolute paths to gpks files! Create lists with 00_Create_productLists.ipynb
 
 platform=$1
-dst=$2
-cd $dst
-echo $dst
-for zf in *; do
-    ogrmerge -f GPKG -o "${platform}_GEOMAR_TID_EPSG3395.gpkg" "*_Coverage.gpkg" -src_geom_type MULTIPOLYGON -overwrite_ds
+ifi=$2
+IFS=$'\n'       
+set -f    
+for f in $(cat < "$ifi"); do
+    ogrmerge -f GPKG -o "${platform}_GEOMAR_TID_EPSG3395.gpkg" "$f" -src_geom_type MULTIPOLYGON -overwrite_ds
 done
